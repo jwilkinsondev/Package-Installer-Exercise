d2c9530986e4f49afa1277aa52c7067700677fdb
jwilkinsondev <jwilkinsondev@gmail.com>
2017-06-12 20:33:56 -0600

    Code cleanup and hardening

--- spec/packageInstallerSpec.js
+++ spec/packageInstallerSpec.js
@@ -1,108 +1,81 @@
 
 describe('package installer exercise specs', () => {
-  var PackageInstaller = require('../src/packageInstaller');
-  var testInstaller
-  beforeEach(() => {
-    testInstaller = new PackageInstaller();
+    var PackageInstaller = require('../src/packageInstaller');
+    var testInstaller;
+    beforeEach(() => {
+        testInstaller = new PackageInstaller();
 
-    spyOn(testInstaller, "setSavedInputString").and.callThrough();
-  });
-  
-  
-  afterEach(() => {
-    testInstaller = null;
-  });
-    
-  
-  describe('Verify that the object is set up correctly', () => {
-    it('should be an object', () => {
-      expect(testInstaller).toEqual(jasmine.any(Object));
-    });  
-  });
+        spyOn(testInstaller, "setSavedInputString").and.callThrough();
+    });
 
-  
-  describe('getters and setters', () => {
-      var result;
-      beforeEach(() => {
-          result = null;
-      });
-      
-      afterEach(() => {
-          result = null;
-      });
-          
-      it('should get savedInputString', () => {
-          result = testInstaller.getSavedInputString();
-          expect(result).toEqual(testInstaller.m_savedInputString);
-      });
-      
-      it('should set savedInputString', () => {
-          testInstaller.setSavedInputString("foo");
-          expect(testInstaller.m_savedInputString).toEqual("foo");
-      });
-          
-          
-  });
-      
-  
-  describe('Handle input', () => {
-      
-      it('should take in input', () => {
-          testInstaller.getRawInput();
-          expect(testInstaller.m_savedInputString.length).toBeGreaterThan(0);
-      });
-      
-      it('should store the input', () => {
-          testInstaller.getRawInput();
-          expect(testInstaller.getSavedInputString().length).toBeGreaterThan(0);
-          expect(testInstaller.setSavedInputString).toHaveBeenCalled();
-      });
-          
-  });
 
-  
-  describe('validate the input', () => {
-      it('should only accept arrays', () => {
-          testInstaller.m_savedInputString = [];
-          expect(testInstaller.validateInput()).toEqual(true);
-          testInstaller.m_savedInputString = "stuff";
-          expect(testInstaller.validateInput()).toEqual(false);
-      });
-  });
+    afterEach(() => {
+        testInstaller = null;
+    });
 
-  
-  describe('generate output', () => {
-      var dependencies;
-      var sortedResults;
-      var result;
-      describe('generate the list of nodes', () => {
-          
-          it('should create as many objects as there are dependencies', () => {
-              dependencies = [ "KittenService: CamelCaser", "CamelCaser: " ]
 
-              result = testInstaller.generateNodes(dependencies);
-              expect(result.length).toEqual(dependencies.length);
-          });
+    describe('Verify that the object is set up correctly', () => {
+        it('should be an object', () => {
+            expect(testInstaller).toEqual(jasmine.any(Object));
+        });
+    });
 
-          
-          it('should have an empty incomingEdges array if the dependency does not depend on anything else', () => {
-              dependencies = ["CamelCaser: "];
-              result = testInstaller.generateNodes(dependencies);
-              expect(result[0].incomingEdges).toEqual([]);
-          });
-      });
-      
-      describe('kahnsSort', () => {
-          
-          it('should return a valid dependency order', () => {
-              dependencies = [ "KittenService: CamelCaser", "CamelCaser: " ];
-              result = testInstaller.kahnsSort(dependencies);
-              expect(result).toEqual([ 'CamelCaser', 'KittenService' ]);
-          });
 
-          
-          it('should return -1 for dependency loops', () => {
-                dependencies =  ["KittenService: ",
+    describe('getters and setters', () => {
+        var result;
+        beforeEach(() => {
+            result = null;
+        });
+
+        afterEach(() => {
+            result = null;
+        });
+
+        it('should get savedInputString', () => {
+            result = testInstaller.getSavedInputString();
+            expect(result).toEqual(testInstaller.m_savedInputString);
+        });
+
+        it('should set savedInputString', () => {
+            testInstaller.setSavedInputString("foo");
+            expect(testInstaller.m_savedInputString).toEqual("foo");
+        });
+    });
+
+    describe('validate the input', () => {
+        it('should only accept arrays', () => {
+            expect(testInstaller.validateInput([])).toEqual(true);
+            expect(testInstaller.validateInput("stuff")).toEqual(false);
+        });
+    });
+
+
+    describe('generate output', () => {
+        var dependencies;
+        var sortedResults;
+        var result;
+        describe('generate the list of nodes', () => {
+            it('should create as many objects as there are dependencies', () => {
+                dependencies = ["KittenService: CamelCaser", "CamelCaser: "]
+
+                result = testInstaller.generateNodes(dependencies);
+                expect(result.length).toEqual(dependencies.length);
+            });
+            it('should have an empty incomingEdges array if the dependency does not depend on anything else', () => {
+                dependencies = ["CamelCaser: "];
+                result = testInstaller.generateNodes(dependencies);
+                expect(result[0].incomingEdges).toEqual([]);
+            });
+        });
+
+        describe('kahnsSort', () => {
+            it('should return a valid dependency order', () => {
+                dependencies = ["KittenService: CamelCaser", "CamelCaser: "];
+                result = testInstaller.kahnsSort(dependencies);
+                expect(result).toEqual(['CamelCaser', 'KittenService']);
+            });
+            it('should return -1 for dependency loops', () => {
+                dependencies = ["KittenService: ",
                     "Leetmeme: Cyberportal",
                     "Cyberportal: Ice",
                     "CamelCaser: KittenService",
@@ -111,36 +84,30 @@ describe('package installer exercise specs', () => {
                 ]
                 result = testInstaller.kahnsSort(dependencies);
                 expect(result).toEqual(-1)
-          });
-      });
-          
-      
-      describe('generate output', () => {
-          
-          it('should handle success correctly', () => {
-              sortedResults = [ 'CamelCaser', 'KittenService' ];
-              result = testInstaller.generateOutput(sortedResults);
-              expect(result).toEqual("CamelCaser, KittenService");
-          });
-          
-          it('should handle loops correctly', () => {
-              sortedResults = -1;
-              result = testInstaller.generateOutput(sortedResults);
-              expect(result).toEqual("The dependency list contained a cycle. Please resolve the cycle and resubmit.")
-          });
-                      
-      });
-              
-  });
-      
-  
-  describe('should take in input and return final output', () => {
-      var dependencies;
-      var result;
+            });
+        });
 
-      
+        describe('generate output', () => {
+            it('should handle success correctly', () => {
+                sortedResults = ['CamelCaser', 'KittenService'];
+                result = testInstaller.generateOutput(sortedResults);
+                expect(result).toEqual("CamelCaser, KittenService");
+            });
+            it('should handle loops correctly', () => {
+                sortedResults = -1;
+                result = testInstaller.generateOutput(sortedResults);
+                expect(result).toEqual("The dependency list contained a cycle. Please resolve the cycle and resubmit.")
+            });
+        });
+    });
+
+
+    describe('should take in input and return final output', () => {
+        var dependencies;
+        var result;
+
         it('should generate a valid response for valid data', () => {
-            dependencies = [ "KittenService: CamelCaser", "CamelCaser: " ]
+            dependencies = ["KittenService: CamelCaser", "CamelCaser: "]
             result = testInstaller.main(dependencies);
             expect(result).toEqual("CamelCaser, KittenService");
 
@@ -154,12 +121,8 @@ describe('package installer exercise specs', () => {
             ]
             result = testInstaller.main(dependencies);
             expect(result).toEqual("Ice, Cyberportal, Leetmeme, Fraudstream, KittenService, CamelCaser");
-
-        
-      });
-          
-      
-      it('should generate a valid response for invalid data', () => {
+        });
+        it('should generate a valid response for invalid data that contains a cycle', () => {
             dependencies = [
                 "KittenService: ",
                 "Leetmeme: Cyberportal",
@@ -171,9 +134,18 @@ describe('package installer exercise specs', () => {
 
             result = testInstaller.main(dependencies);
             expect(result).toEqual("The dependency list contained a cycle. Please resolve the cycle and resubmit.");
-      });
-
-  });
-        
+        });
+        it('should return a generic error message for garbage input', () => {
+            result = testInstaller.main(null);
+            expect(result).toEqual("An invalid list of dependencies was submitted. Please fix the dependencies and resubmit.");
+            result = testInstaller.main(undefined);
+            expect(result).toEqual("An invalid list of dependencies was submitted. Please fix the dependencies and resubmit.");
+            result = testInstaller.main("foo");
+            expect(result).toEqual("An invalid list of dependencies was submitted. Please fix the dependencies and resubmit.");
+            result = testInstaller.main(42);
+            expect(result).toEqual("An invalid list of dependencies was submitted. Please fix the dependencies and resubmit.");
+            result = testInstaller.main({});
+            expect(result).toEqual("An invalid list of dependencies was submitted. Please fix the dependencies and resubmit.");
+        });
+    });
 });
-  
--- src/packageInstaller.js
+++ src/packageInstaller.js
@@ -1,6 +1,3 @@
-var inputString = "some string";
-
-
 
 function PackageInstaller() {
     this.m_savedInputString;
@@ -8,45 +5,42 @@ function PackageInstaller() {
 
 /* getters and setters*/
 
-PackageInstaller.prototype.getSavedInputString = function(){
+PackageInstaller.prototype.getSavedInputString = function () {
     return this.m_savedInputString;
 }
-PackageInstaller.prototype.setSavedInputString = function(input){
+PackageInstaller.prototype.setSavedInputString = function (input) {
     this.m_savedInputString = input;
 }
 //------------------------------------------------------------------------------------------------------
 
 
-PackageInstaller.prototype.getRawInput = function(){
-    this.setSavedInputString(inputString);
-}
 
-PackageInstaller.prototype.validateInput = function(){
-    var dependencies = this.getSavedInputString();
-    if(!Array.isArray(dependencies)){
+PackageInstaller.prototype.validateInput = function (input) {
+
+    if (!Array.isArray(input)) {
         // the input isn't even an array so it's not valid
         return false;
     }
     return true;
 }
 
-PackageInstaller.prototype.kahnsSort = function(input){
+PackageInstaller.prototype.kahnsSort = function (input) {
     var result = [];
     var nextNodes = [];
     var currentNode;
     var nodes = this.generateNodes(input);
-    var newNodes = []; 
+    var newNodes = [];
     var nodesCount = nodes.length;
     var validationLength = nodesCount;
     var i;
     var j;
     var currentIncomingEdges;
-    for(i=0; i < nodesCount; i++){
+    for (i = 0; i < nodesCount; i++) {
         // if a node isn't dependent on anything, add it to the list of next avalible nodes
-        if(nodes[i].incomingEdges == 0){
+        if (nodes[i].incomingEdges == 0) {
             nextNodes.push(nodes[i]);
         }
-        else{
+        else {
             // make a new list since it's a bad idea to delete from the same list we're looping over
             newNodes.push(nodes[i])
         }
@@ -55,88 +49,107 @@ PackageInstaller.prototype.kahnsSort = function(input){
     nodes = newNodes;
 
     // while nextNodes isn't empty
-    while(nextNodes.length > 0){
+    while (nextNodes.length > 0) {
         currentNode = nextNodes.pop();
         result.push(currentNode.name);
 
 
         // for each node
-        for(i = 0; i < nodes.length; i++){
+        for (i = 0; i < nodes.length; i++) {
             currentIncomingEdges = nodes[i].incomingEdges;
-            for(j = currentIncomingEdges.length - 1; j >= 0; j--){
+            for (j = currentIncomingEdges.length - 1; j >= 0; j--) {
                 // if the current node is an incoming edge
-                if(currentIncomingEdges[j] === currentNode.name){
+                if (currentIncomingEdges[j] === currentNode.name) {
                     // remove the current node from the incoming edges
-                   currentIncomingEdges.splice(j, 1);
+                    currentIncomingEdges.splice(j, 1);
                 }
             }
             // check to see if all of its dependencies are now resolved
-            if(nodes[i].incomingEdges.length === 0){
+            if (nodes[i].incomingEdges.length === 0) {
                 nextNodes.push(nodes[i])
                 // remove the node from the list
                 nodes.splice(i, 1);
             }
-            
+
         }
     }
-    if(result.length === validationLength ){
+    if (result.length === validationLength) {
         return result;
     }
-    else{
+    else {
         return -1;
     }
 
 }
 
-PackageInstaller.prototype.generateNodes = function(input){
+PackageInstaller.prototype.generateNodes = function (input) {
     var result = [];
     var workingNode = {
         name: "",
-        incomingEdges:[]
+        incomingEdges: []
     };
-    input.forEach(function(element) {
+    input.forEach(function (element) {
         workingNode.name = element.substring(0, element.indexOf(":"));
-        
+
         // only push an edge if one exists, otherwise, leave the edge array empty
-        if(element.substring(element.indexOf(":") + 2).length){
+        if (element.substring(element.indexOf(":") + 2).length) {
             workingNode.incomingEdges.push(element.substring(element.indexOf(":") + 2));
         };
         result.push(workingNode);
         workingNode = {
             name: "",
-            incomingEdges:[]
+            incomingEdges: []
         }
     }, this);
     return result;
 }
 
-PackageInstaller.prototype.generateOutput = function(input){
-    if(input === -1){
+PackageInstaller.prototype.generateOutput = function (input) {
+    if (input === -1) {
         return "The dependency list contained a cycle. Please resolve the cycle and resubmit.";
     }
 
     return input.join(', ');
 }
 
-PackageInstaller.prototype.main = function(input){
-    var sortedNodes = this.kahnsSort(input);
-    var result = this.generateOutput(sortedNodes);
-    return result;
+PackageInstaller.prototype.main = function (input) {
+    var sortedNodes;
+    var result;
+    // make sure that the input is actually an array
+    if (this.validateInput(input)) {
+        sortedNodes = this.kahnsSort(input);
+        result = this.generateOutput(sortedNodes);
+        return result;
+    }
+    else {
+        return "An invalid list of dependencies was submitted. Please fix the dependencies and resubmit."
+    }
+
 }
 
 
 var testInstaller = new PackageInstaller();
-var dependencies =   [
- "KittenService: ",
- "Leetmeme: Cyberportal",
- "Cyberportal: Ice",
- "CamelCaser: KittenService",
- "Fraudstream: Leetmeme",
- "Ice: "
+var valid1 = ["KittenService: CamelCaser", "CamelCaser: "];
+var valid2 = [
+    "KittenService: ",
+    "Leetmeme: Cyberportal",
+    "Cyberportal: Ice",
+    "CamelCaser: KittenService",
+    "Fraudstream: Leetmeme",
+    "Ice: "
+]
+var invalid1 = [
+    "KittenService: ",
+    "Leetmeme: Cyberportal",
+    "Cyberportal: Ice",
+    "CamelCaser: KittenService",
+    "Fraudstream: ",
+    "Ice: Leetmeme"
 ]
 
-var result = testInstaller.kahnsSort(dependencies);
-result = testInstaller.generateOutput(result)
-console.log(result)
 
+console.log(testInstaller.main(valid1));
+console.log(testInstaller.main(valid2));
+console.log(testInstaller.main(invalid1));
+console.log(testInstaller.main(null));
 module.exports = PackageInstaller;
