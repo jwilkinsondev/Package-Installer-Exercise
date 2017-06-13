9c4a714ed89e96f4d2a0b03d972c1dc8a9b9c92a
jwilkinsondev <jwilkinsondev@gmail.com>
2017-06-12 18:09:59 -0600

    checking in node generation

--- spec/packageInstallerSpec.js
+++ spec/packageInstallerSpec.js
@@ -1,13 +1,11 @@
-  it("contains spec with an expectation", function() {
-    expect(true).toBe(true);
-  });
-
 
 describe('package installer exercise specs', () => {
   var PackageInstaller = require('../src/packageInstaller');
   var testInstaller
   beforeEach(() => {
     testInstaller = new PackageInstaller();
+
+    spyOn(testInstaller, "setSavedInputString").and.callThrough();
   });
   
   
@@ -24,6 +22,121 @@ describe('package installer exercise specs', () => {
     });
       
   });
+
+  
+  describe('getters and setters', () => {
+      var result;
+      beforeEach(() => {
+          result = null;
+      });
+      
+      afterEach(() => {
+          result = null;
+      });
+          
+      it('should get savedInputString', () => {
+          result = testInstaller.getSavedInputString();
+          expect(result).toEqual(testInstaller.m_savedInputString);
+      });
+      
+      it('should set savedInputString', () => {
+          testInstaller.setSavedInputString("foo");
+          expect(testInstaller.m_savedInputString).toEqual("foo");
+      });
+          
+          
+  });
+      
+  
+  describe('Handle input', () => {
+      
+      it('should take in input', () => {
+          testInstaller.getRawInput();
+          expect(testInstaller.m_savedInputString.length).toBeGreaterThan(0);
+      });
+      
+      it('should store the input', () => {
+          testInstaller.getRawInput();
+          expect(testInstaller.getSavedInputString().length).toBeGreaterThan(0);
+          expect(testInstaller.setSavedInputString).toHaveBeenCalled();
+      });
+          
+  });
+
+  
+  xdescribe('validate the input', () => {
+      
+      xit('should only accept arrays', () => {
+          testInstaller.m_savedInputString = [];
+          expect(testInstaller.validateInput()).toEqual(true);
+          testInstaller.m_savedInputString = "stuff";
+          expect(testInstaller.validateInput()).toEqual(false);
+      });
+      
+      xit('should only accept input that does not have dependency loops', () => {
+          var valid =  [
+                "KittenService: ",
+                "Leetmeme: Cyberportal",
+                "Cyberportal: Ice",
+                "CamelCaser: KittenService",
+                "Fraudstream: Leetmeme",
+                "Ice: "
+            ];
+          var invalid = [
+                "KittenService: ",
+                "Leetmeme: Cyberportal",
+                "Cyberportal: Ice",
+                "CamelCaser: KittenService",
+                "Fraudstream: ",
+                "Ice: Leetmeme"
+            ]
+          expect(testInstaller.checkForDependencyLoops(valid)).toBe(true);
+          expect(testInstaller.checkForDependencyLoops(invalid)).toBe(false);
+      });
+          
+          
+  });
+
+  
+  describe('generate output', () => {
+      var dependencies;
+      var result;
+      describe('generate the list of nodes', () => {
+          
+          it('should create as many objects as there are dependencies', () => {
+              dependencies = [ "KittenService: CamelCaser", "CamelCaser: " ]
+
+              result = testInstaller.generateNodes(dependencies);
+              expect(result.length).toEqual(dependencies.length);
+          });
+
+          
+          it('should have an empty incomingEdges array if the dependency does not depend on anything else', () => {
+              dependencies = ["CamelCaser: "];
+              result = testInstaller.generateNodes(dependencies);
+              expect(result[0].incomingEdges).toEqual([]);
+          });
+      });
+      
+      describe('kahnsSort', () => {
+          
+          it('should return a valid dependency order', () => {
+              dependencies = [ "KittenService: CamelCaser", "CamelCaser: " ];
+              result = testInstaller.kahnsSort(dependencies);
+              expect(result).toEqual("CamelCaser, KittenService");
+          });
+
+          
+          it('should handle dependency loops', () => {
+              
+          });
+              
+              
+      });
+          
+          
+  });
+      
     
 });
   
--- src/packageInstaller.js
+++ src/packageInstaller.js
@@ -1,9 +1,70 @@
+var inputString = "some string";
 
-function PackageInstaller() {};
 
-PackageInstaller.prototype.sayHi = function() {
-    return "Hi"
+
+function PackageInstaller() {
+    this.m_savedInputString;
 };
+
+/* getters and setters*/
+
+PackageInstaller.prototype.getSavedInputString = function(){
+    return this.m_savedInputString;
+}
+PackageInstaller.prototype.setSavedInputString = function(input){
+    this.m_savedInputString = input;
+}
+//------------------------------------------------------------------------------------------------------
+
+
+PackageInstaller.prototype.getRawInput = function(){
+    this.setSavedInputString(inputString);
+}
+
+PackageInstaller.prototype.validateInput = function(){
+    var dependencies = this.getSavedInputString();
+    if(!Array.isArray(dependencies)){
+        // the input isn't even an array so it's not valid
+        return false;
+    }
+    if(this.checkForDependencyLoops(dependencies)){
+        // there are loops in the dependency so it's not valid
+        return false;
+    }
+
+    return true;
+}
+
+PackageInstaller.prototype.kahnsSort = function(dependencies){
+
+}
+
+PackageInstaller.prototype.generateNodes = function(input){
+    var result = [];
+    var workingNode = {
+        name: "",
+        incomingEdges:[]
+    };
+    input.forEach(function(element) {
+        workingNode.name = element.substring(0, element.indexOf(":"));
+        
+        // only push an edge if one exists, otherwise, leave the edge array empty
+        if(element.substring(element.indexOf(":") + 2).length){
+            workingNode.incomingEdges.push(element.substring(element.indexOf(":") + 2));
+        };
+        result.push(workingNode);
+        workingNode = {
+            name: "",
+            incomingEdges:[]
+        }
+    }, this);
+    console.log(result);
+    return result;
+}
+
+
+
+
 
 
 module.exports = PackageInstaller;
